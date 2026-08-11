8a32c5738ceac6fe95959f2e0d95a589eaf42606
gasshara <gasshara@gmail.com>
2026-08-11 11:25:06 +0200

    new directives

--- js/factory/colorutils.js
+++ js/factory/colorutils.js
@@ -404,7 +404,7 @@ function optimizeLightness(hue, sat, direction, bgRgb, minContrast) {
         const rgb = hslToRgb(hue, sat, mid);
         const hex = rgbToHex(rgb);
         const ratio = contrastRatio(hex, bgRgb);
-	console.log({mid, rgb, hex, ratio, i, minContrast});
+	//console.log({mid, rgb, hex, ratio, i, minContrast});
 
         if (ratio >= minContrast) {
             if (ratio > bestRatio) {
