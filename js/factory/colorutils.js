82320060ab2dba64803e71aab68269b8dc2186b0
gasshara <gasshara@gmail.com>
2026-08-11 14:27:34 +0200

    new directives

--- js/factory/colorutils.js
+++ js/factory/colorutils.js
@@ -210,8 +210,8 @@ export function computeForeground(desired, bg, minRatio=4.5) {
 }
 export function emphasize(color, bg, intensity=1) {
     console.log({color, bg});
-  const fgHsl=rgbToHsl(hexToRgb(color));
-  const bgHsl=rgbToHsl(hexToRgb(bg));
+  const fgHsl=rgbToHsl(hexToRgb(""+color));
+  const bgHsl=rgbToHsl(hexToRgb(""+bg));
     console.log({fgHsl, bgHsl});
   if(Math.abs(fgHsl.h-bgHsl.h)<30) fgHsl.h=(fgHsl.h+30)%360;
   fgHsl.s=Math.min(100,fgHsl.s+15*intensity);
