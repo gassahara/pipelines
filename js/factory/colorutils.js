4ee3a1541c2e254129ebb283ac1b87774fdfaf57
gasshara <gasshara@gmail.com>
2026-08-11 10:37:17 +0200

    new directives

--- js/factory/colorutils.js
+++ js/factory/colorutils.js
@@ -397,7 +397,7 @@ function optimizeLightness(hue, sat, direction, bgRgb, minContrast) {
         high = 25;  // towards darker
     }
 
-    let bestLight = null;
+    let bestLight = 0;
     let bestRatio = Infinity;
 
     for (let i = 0; i < 120; i++) {   // binary search, 20 iterations max
