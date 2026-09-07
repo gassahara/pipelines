c5368b6b1775f60dda7e304f93249c8c66a5ffce
gasshara <gasshara@gmail.com>
2026-09-07 05:43:10 +0200

    REFCATOR: consolidation

--- js/bootloader.js
+++ js/bootloader.js
@@ -130,7 +130,7 @@ function runpipelineboot(loadprogram, report, manifest) {
   loadnext();
 }
 
-var pipelinesbase = 'https://gassahara.github.io/pipelines/';
+var pipelinesbase = 'https://gassahara.github.io/pipelines/js/';
 
 function bootpipeline(ondone) {
   function loadscript(entry, done) {
